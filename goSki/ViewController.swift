885245202ecf8c8fc66fe1d20e62bcd370d53e7d
hhr <honghu_haoran@163.com>
2019-01-27 13:50:27 -0500

    Initial Commit

---
+++ goSki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goSki
+//
+//  Created by Haoran Hu on 1/27/19.
+//  Copyright © 2019 hhr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
