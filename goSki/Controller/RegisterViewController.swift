5d5e484d3902e746a3dd9487b4772b0ebccc111e
hhr <honghu_haoran@163.com>
2019-01-29 17:07:10 -0500

    RegisterViewController: 	import SVProgressHUD to display animation while getting data from server

--- goSki/Controller/RegisterViewController.swift
+++ goSki/Controller/RegisterViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
@@ -20,16 +21,35 @@ class RegisterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func displayUIAlert(title: String, text: String, buttonText: String){
+        let alert = UIAlertController(title: "\(title)", message: "\(text)", preferredStyle: .alert )
+        alert.addAction(UIAlertAction(title: NSLocalizedString("\(buttonText))", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"\(buttonText)\" alert occured.")
+        }))
+        self.present(alert,animated: true,completion: nil)
+    }
+    
     @IBAction func registerPressed(_ sender: UIButton) {
+        SVProgressHUD.show()
+        //user entered two different passwords
         if passWordTextField.text != confirmPassWordTextField.text{
             print("Passwords entered are different.")
+            displayUIAlert(title: "Registration Failed", text: "You've entered two different password.", buttonText: "OK")
+            SVProgressHUD.dismiss()
+        // try connect to server and register
         }else{
             print("go register")
             Auth.auth().createUser(withEmail: emailTextField.text!, password: passWordTextField.text!) {
                 (authDataResult, error) in
+                // received eorror
                 if error != nil{
+                    
                     print("!!! error", error!)
+                    self.displayUIAlert(title: "registration failed", text: "\(error!.localizedDescription)", buttonText: "OK")
+                    SVProgressHUD.dismiss()
+                //registration successful
                 }else{
+                    SVProgressHUD.dismiss()
                     print("registration successful!")
                 }
             }
